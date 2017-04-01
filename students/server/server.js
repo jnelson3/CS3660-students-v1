b370576b03923082c0fe008ad48dda6ad5f5ad78
Jonathan Nelson <nelsonj3@gmail.com>
2017-04-01 00:15:45 -0600

    added middleware colors, winston, and nconf

--- students/server/server.js
+++ students/server/server.js
@@ -1,26 +1,35 @@
-console.log('Loading Server');
+console.log('info: Loading Server');
 
 //load main modules
 var express = require('express');
+var colors = require('colors');
+var logger = require('winston');
+var nconf = require('nconf');
 var fs = require('fs');
 
+
 //load express milddleware mdoules
-var logger = require('morgan');
+var morgan = require('morgan');
 var compression = require('compression');
 var favicon = require('serve-favicon');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var WEB = path.resolve('../../students/web'); // __dirname is the directory where the application is running from
+var WEB = path.resolve('../web'); // __dirname is the directory where the application is running from
 //var WEB = __dirname.replace('server', 'web');
 var SERVER = __dirname; // __dirname is the directory where the application is running from
+
+//get config info
+nconf.argv().file({file:SERVER + '/config.conf'});
+nconf.set('foo', 'bar');
 
 //create express app
 var app = express();
 
-//insert middleware
+//add winston file logger
+logger.add(logger.transports.File, { filename: SERVER + '/log.log'});
 
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(compression());
 app.use(favicon(WEB + '/img/favicon.ico'));
 app.use(bodyParser.json());
@@ -28,85 +37,85 @@ app.use(bodyParser.json());
 //REST end points
 // create
 app.post('/api/v1/students', function(req, res) {
-   var data = req.body;
-   if (!data) {
-      res.sendStatus(400);
-      return;
-   }
+    var data = req.body;
+    if (!data) {
+        res.sendStatus(400);
+        return;
+    }
 
-   fs.readdir(`${__dirname}/students`, function(err, files) {
-      if (err) {
-         res.sendStatus(500);
-         return;
-      };
-
-      var fileList = files.map(fileName => fileName.replace('.json', ''));
-
-      var id = pad(parseInt(fileList[fileList.length - 1]) + 1);
-      data.id = id;
-
-      fs.writeFile(`${__dirname}/students/${id}.json`, JSON.stringify(data, null, 2), 'utf8', function(err) {
-         if (err) {
+    fs.readdir(`${__dirname}/students`, function(err, files) {
+        if (err) {
             res.sendStatus(500);
             return;
-         };
+        };
 
-         res.status(201).json(id); // send status 200 and fileList
-      });
-   });
+        var fileList = files.map(fileName => fileName.replace('.json', ''));
+
+        var id = pad(parseInt(fileList[fileList.length - 1]) + 1);
+        data.id = id;
+
+        fs.writeFile(`${__dirname}/students/${id}.json`, JSON.stringify(data, null, 2), 'utf8', function(err) {
+            if (err) {
+                res.sendStatus(500);
+                return;
+            };
+
+            res.status(201).json(id); // send status 200 and fileList
+        });
+    });
 });
 
 // list
 app.get('/api/v1/students/students.json', function(req, res) {
-   fs.readdir(`${__dirname}/students`, function(err, files) {
-      if (err) {
-         res.sendStatus(404);
-      }
+    fs.readdir(`${__dirname}/students`, function(err, files) {
+        if (err) {
+            res.sendStatus(404);
+        }
 
-      var fileList = files.map(fileName => fileName.replace('.json', ''));
-      res.json(fileList); // send status 200 and fileList
-   });
+        var fileList = files.map(fileName => fileName.replace('.json', ''));
+        res.json(fileList); // send status 200 and fileList
+    });
 });
 
 // read
 app.get('/api/v1/students/:id.json', function(req, res) {
-   var id = req.params.id;
-   fs.readFile(`${__dirname}/students/${id}.json`, 'utf8', function(err, data) {
-      if (err) {
-         res.sendStatus(404);
-      }
-      
-      res.set('id', req.params.id);
+    var id = req.params.id;
+    fs.readFile(`${__dirname}/students/${id}.json`, 'utf8', function(err, data) {
+        if (err) {
+            res.sendStatus(404);
+        }
 
-      res.status(200).json(JSON.parse(data)); // send status 200 and fileList
-   });
+        res.set('id', req.params.id);
+
+        res.status(200).json(JSON.parse(data)); // send status 200 and fileList
+    });
 
 });
 
 // update
 app.put('/api/v1/students/:id.json', function(req, res) {
-   var id = req.params.id;
-   var data = JSON.stringify(req.body, null, 2);
-   
-   console.log('content-type = ' + req.get('Content-Type'));
-   console.log('data = ' + JSON.stringify(req.body));
+    var id = req.params.id;
+    var data = JSON.stringify(req.body, null, 2);
 
-   fs.writeFile(`${__dirname}/students/${id}.json`, data, 'utf8', function(err) {
-      if (err) throw err;
+    logger.debug(colors.green('content-type = ' + req.get('Content-Type')));
+    logger.debug(colors.green('data = ' + JSON.stringify(req.body)));
 
-      res.sendStatus(204); // send status 200 and fileList
-   });
+    fs.writeFile(`${__dirname}/students/${id}.json`, data, 'utf8', function(err) {
+        if (err) throw err;
+
+        res.sendStatus(204); // send status 200 and fileList
+    });
 
 });
 
 // delete
 app.delete('/api/v1/students/:id.json', function(req, res) {
-   var id = req.params.id;
-   fs.unlink(`${__dirname}/students/${id}.json`, function(err) {
-      if (err) throw err;
+    var id = req.params.id;
+    fs.unlink(`${__dirname}/students/${id}.json`, function(err) {
+        if (err) throw err;
 
-      res.sendStatus(204); // send status 200 and fileList
-   });
+        res.sendStatus(204); // send status 200 and fileList
+    });
 
 });
 
@@ -118,26 +127,26 @@ app.delete('/api/v1/students/:id.json', function(req, res) {
 app.use(express.static(WEB));
 //app.use(express.static(SERVER));
 app.get('*', function(req, res) {
-   res.status(404).sendFile(WEB + '/404.html');
+    res.status(404).sendFile(WEB + '/404.html');
 });
 
 var server = app.listen(80, '0.0.0.0');
 
-console.log('Server is listening');
+logger.info('Server is listening');
 
 function gracefullShutdown() {
-   console.log('\nStarting Shutdown');
-   server.close(function() {
-      console.log('\nShutdown Complete');
-   });
+    logger.info('Starting Shutdown');
+    server.close(function() {
+        logger.info('Shutdown Complete');
+    });
 }
 
 process.on('SIGTERM', function() { //kill (terminate)
-   gracefullShutdown();
+    gracefullShutdown();
 });
 
 process.on('SIGINT', function() { //Ctrl+C (interrupt)
-   gracefullShutdown();
+    gracefullShutdown();
 });
 
 //SIGKILL (kill -9) can't be caught by any process, including node
@@ -145,6 +154,6 @@ process.on('SIGINT', function() { //Ctrl+C (interrupt)
 // Yes I just copied and pasted the code from Canvas. 
 
 function pad(num) {
-   var s = "0000" + num;
-   return s.substr(s.length - 4);
+    var s = "0000" + num;
+    return s.substr(s.length - 4);
 }
